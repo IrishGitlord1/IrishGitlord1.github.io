862e4476bf1779efd204e1a6faeb4f8011d732fe
IrishCinnamon <18431742+IrishGitlord1@users.noreply.github.com>
2019-09-20 12:51:09 -0500

    Delete elements.js

--- elements.js
+++
@@ -1,3 +0,0 @@
-export let elementsJSON = '{ "elements": [ ' + 
-   '{"name": "Hydrogen", "symbol": "H", "weight": "1.008", "density": "0.09", "discovery": "1776" },' + 
-   '{"name": "", "symbol": "", "weight": "", "density": "", "discovery": ""}]}';
