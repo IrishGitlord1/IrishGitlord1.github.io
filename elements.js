adf9416f1dc26447090abbdf51e4bdff20409b30
IrishCinnamon <18431742+IrishGitlord1@users.noreply.github.com>
2019-09-20 12:38:08 -0500

    Add files via upload

---
+++ elements.js
@@ -0,0 +1,3 @@
+export let elementsJSON = '{ "elements": [ ' + 
+   '{"name": "Hydrogen", "symbol": "H", "weight": "1.008", "density": "0.09", "discovery": "1776" },' + 
+   '{"name": "", "symbol": "", "weight": "", "density": "", "discovery": ""}]}';
--- siteJS.js
+++ siteJS.js
@@ -1,3 +1,5 @@
+import { elementsJSON } from "pt";
+
 function getCookie(cname) {
 	var name = cname + "=";
 	var ca = document.cookie.split(';');
@@ -34,23 +36,7 @@ function setDarkmode() {
 	}
 }
 
-function loadJSON(callback) {   
-
-    var xobj = new XMLHttpRequest();
-        xobj.overrideMimeType("application/json");
-    xobj.open('GET', 'elements.json', true);
-    xobj.onreadystatechange = function () {
-          if (xobj.readyState == 4 && xobj.status == "200") {
-            callback(xobj.responseText);
-          }
-    };
-    xobj.send(null);  
- }
- 
-
 function cardifyElements() {
-	loadJSON(function(elements) {
-		var elementsJSON = JSON.parse(elements);
-		console.log(elements);
-	 });
+	var elements = JSON.parse(elementsJSON);
+	console.log(elements.elements[0].name + " " + elements.elements[0].symbol);
 }
