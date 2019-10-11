e34c08e070428fb053a13525977af4bf55a3d990
IrishCinnamon <18431742+IrishGitlord1@users.noreply.github.com>
2019-10-10 21:44:58 -0500

    Add files via upload

--- siteJS.js
+++ siteJS.js
@@ -1,8 +1,8 @@
 var elements = [
-        { "number": "1", "name": "Hydrogen", "symbol": "H", "weight": "1.008", "state": "gas", "groups": "Other nonmetals" },
-        { "number": "2", "name": "Helium", "symbol": "He", "weight": "4.003", "state": "gas", "groups": "Noble Gases" },
-        { "number": "3", "name": "Lithium", "symbol": "Li", "weight": "6.941", "state": "solid", "groups": "Alkali Metals" },
-        { "number": "4", "name": "Beryllium", "symbol": "Be", "weight": "9.012", "state": "solid", "groups": "Alkaline Earth Metals" },
+      { "number": "1", "name": "Hydrogen", "symbol": "H", "weight": "1.008", "state": "gas", "groups": "Other nonmetals" },
+      { "number": "2", "name": "Helium", "symbol": "He", "weight": "4.003", "state": "gas", "groups": "Noble Gases" },
+      { "number": "3", "name": "Lithium", "symbol": "Li", "weight": "6.941", "state": "solid", "groups": "Alkali Metals" },
+      { "number": "4", "name": "Beryllium", "symbol": "Be", "weight": "9.012", "state": "solid", "groups": "Alkaline Earth Metals" },
     	{ "number": "5", "name": "Boron", "symbol": "B", "weight": "10.811", "state": "solid", "groups": "Metalloids" },
 	    { "number": "6", "name": "Carbon", "symbol": "C", "weight": "12.011", "state": "solid", "groups": "Other nonmetals" },
 	    { "number": "7", "name": "Nitrogen", "symbol": "N", "weight": "14.007", "state": "gas", "groups": "Other nonmetals" },
@@ -180,3 +180,113 @@ function showSlides(n) {
       break;
   }
 }
+
+var url_string = window.location.href;
+var url = new URL(url_string);
+var searchString = url.searchParams.get("search_bar");
+var searchNumRes = parseInt(searchString);
+var searchSymRes = elements.filter(obj => {
+  return obj.symbol == searchString
+})
+var searchNmeRes = elements.filter(obj => {
+  return obj.name == searchString
+})
+
+function searchFilter() {
+  if (window.location.href !== null && isNaN(searchNumRes) !== true) {
+      slideIndex = searchNumRes;
+      showSlides(slideIndex);
+      $(document).keypress(function(e) {
+        if(e.keyCode == 13) {
+          slideIndex = searchNumRes;
+          showSlides(slideIndex);
+        }
+      });
+  } else if (searchSymRes !== null && searchSymRes.length !== 0) {
+      searchNumFromSymRes = parseInt(searchSymRes[0].number);
+      slideIndex = searchNumFromSymRes;
+      showSlides(slideIndex);
+  } else if (searchNmeRes !== null && searchNmeRes.length !== 0) {
+      searchNumFromNmeRes = parseInt(searchNmeRes[0].number);
+      slideIndex = searchNumFromNmeRes;
+      showSlides(slideIndex);
+  }
+}
+
+function autocomplete(inp, arr) {
+  /*the autocomplete function takes two arguments,
+  the text field element and an array of possible autocompleted values:*/
+  /*execute a function when someone writes in the text field:*/
+  inp.addEventListener("input", function(e) {
+      var a, b, i, val = this.value;
+      /*close any already open lists of autocompleted values*/
+      closeAllLists();
+      if (!val) { return false;}
+      currentFocus = -1;
+      /*create a DIV element that will contain the items (values):*/
+      a = document.createElement("DIV");
+      a.setAttribute("id", this.id + "autocomplete-list");
+      a.setAttribute("class", "autocomplete-items");
+      /*append the DIV element as a child of the autocomplete container:*/
+      this.parentNode.appendChild(a);
+      /*for each item in the array...*/
+      var key;
+      for (key in arr) {
+        if (arr.hasOwnProperty(key) && !isNaN(parseInt(key, 10))) {
+        value = arr[key];
+        /*check if the item starts with the same letters as the text field value:*/
+        if (value.name.substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+          /*create a DIV element for each matching element:*/
+          b = document.createElement("DIV");
+          /*make the matching letters bold:*/
+          b.innerHTML = "<strong>" + value.name.substr(0, val.length) + "</strong>";
+          b.innerHTML += value.name.substr(val.length);
+          /*insert a input field that will hold the current array item's value:*/
+          b.innerHTML += "<input type='hidden' value='" + value.name + "'>";
+          /*execute a function when someone clicks on the item value (DIV element):*/
+          b.addEventListener("click", function(e) {
+              /*insert the value for the autocomplete text field:*/
+              inp.value = this.getElementsByTagName("input")[0].value;
+              /*close the list of autocompleted values,
+              (or any other open lists of autocompleted values:*/
+              closeAllLists();
+          });
+          a.appendChild(b);
+        }
+      }
+      }
+  });
+
+  function addActive(x) {
+    /*a function to classify an item as "active":*/
+    if (!x) return false;
+    /*start by removing the "active" class on all items:*/
+    removeActive(x);
+    if (currentFocus >= x.length) currentFocus = 0;
+    if (currentFocus < 0) currentFocus = (x.length - 1);
+    /*add class "autocomplete-active":*/
+    x[currentFocus].classList.add("autocomplete-active");
+  }
+  function removeActive(x) {
+    /*a function to remove the "active" class from all autocomplete items:*/
+    for (var i = 0; i < x.length; i++) {
+      x[i].classList.remove("autocomplete-active");
+    }
+  }
+  function closeAllLists(elmnt) {
+    /*close all autocomplete lists in the document,
+    except the one passed as an argument:*/
+    var x = document.getElementsByClassName("autocomplete-items");
+    for (var i = 0; i < x.length; i++) {
+      if (elmnt != x[i] && elmnt != inp) {
+        x[i].parentNode.removeChild(x[i]);
+      }
+    }
+  }
+  /*execute a function when someone clicks in the document:*/
+  document.addEventListener("click", function (e) {
+      closeAllLists(e.target);
+  });
+}
+
+autocomplete(document.getElementById("myInput"), elements);
